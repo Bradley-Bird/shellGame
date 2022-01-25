6d2111ed4855c559966ee6d148a8fda087d78c6a
Bradley-Bird <Bradleybird2@icloud.com>
2022-01-24 18:12:05 -0800

    grabbed ids from dom, added counter html

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // import functions and grab DOM elements
+const shellOne = document.getElementById('buttonOne');
+const shellTwo = document.getElementById('buttonTwo');
+const shellThree = document.getElementById('buttonThree');
 
+const shellOneContainer = document.getElementById('shell-1');
+const shellTwoContainer = document.getElementById('shell-2');
+const shellThreeContainer = document.getElementById('shell-3');
+
+const winEL = document.getElementById('win');
+const loseEl = document.getElementById('lose');
+const totalEl = document.getElementById('total');
 // let state
+const ballLocation = []
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// set event listeners
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
