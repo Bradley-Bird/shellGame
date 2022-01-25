502b3dd12bde59789d2a2f0fd53b1bba503962eb
Bradley-Bird <Bradleybird2@icloud.com>
2022-01-24 19:19:32 -0800

    added event listener to add ball class at random

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // import functions and grab DOM elements
-const shellOne = document.getElementById('buttonOne');
-const shellTwo = document.getElementById('buttonTwo');
-const shellThree = document.getElementById('buttonThree');
+const shellOneButton = document.getElementById('buttonOne');
+const shellTwoButton = document.getElementById('buttonTwo');
+const shellThreeButton = document.getElementById('buttonThree');
 
 const shellOneContainer = document.getElementById('shell-1');
 const shellTwoContainer = document.getElementById('shell-2');
@@ -10,10 +10,30 @@ const shellThreeContainer = document.getElementById('shell-3');
 const winEL = document.getElementById('win');
 const loseEl = document.getElementById('lose');
 const totalEl = document.getElementById('total');
+
+
 // let state
-const ballLocation = []
+let win = 0;
+let total = 0;
 
 // set event listeners
+shellOneButton.addEventListener('click', () => {
+  //reset styles
+  shellOneContainer.classList.remove('ball')
+  shellTwoContainer.classList.remove('ball')
+  shellThreeContainer.classList.remove('ball')
+  //put the ball somewhere
+  const ballLocation = Math.ceil(Math.random() * 3);
+  //console.log('click', ballLocation)
+  //define what to do if the ball is on this click
+    if (ballLocation === 1) {
+        shellOneContainer.classList.add('ball');
+    } else if (ballLocation === 2) {
+        shellTwoContainer.classList.add('ball');
+    } else {
+        shellThreeContainer.classList.add('ball');
+    }
+});
 // get user input
 // use user input to update state
 // update DOM to reflect the new state
